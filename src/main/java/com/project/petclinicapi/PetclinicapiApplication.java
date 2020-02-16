e94af00c66f12935b96751cc3466e81793defcd7
lukelee <lukelee01@gmail.com>
2020-02-15 22:37:01 -0500

    Initial commit

---
+++ src/main/java/com/project/petclinicapi/PetclinicapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.petclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicapiApplication.class, args);
+    }
+
+}
