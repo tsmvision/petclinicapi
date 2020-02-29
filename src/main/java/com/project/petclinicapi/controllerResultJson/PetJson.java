47ebeab92e4b2135d96e2c6ebab7278587095f8f
lukelee <lukelee01@gmail.com>
2020-02-29 15:57:30 -0500

    remove unused imports.

--- src/main/java/com/project/petclinicapi/controllerResultJson/PetJson.java
+++ src/main/java/com/project/petclinicapi/controllerResultJson/PetJson.java
@@ -1,8 +1,5 @@
 package com.project.petclinicapi.controllerResultJson;
 
-import com.project.petclinicapi.model.Owner;
-import com.project.petclinicapi.model.PetType;
-import com.project.petclinicapi.model.Visit;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
--- src/main/java/com/project/petclinicapi/controllerResultJson/PetJsonOwner.java
+++ src/main/java/com/project/petclinicapi/controllerResultJson/PetJsonOwner.java
@@ -1,7 +1,4 @@
 package com.project.petclinicapi.controllerResultJson;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class PetJsonOwner extends PersonJson {
 }
